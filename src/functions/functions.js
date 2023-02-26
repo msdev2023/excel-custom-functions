fed7832d01be621b3c581fb3dcbf63e503846e58
msdev2023 <msdev2023@outlook.com>
2023-02-26 21:07:01 +0800

    fetch xueqiu api to get data

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,66 +1,51 @@
 ﻿/* global clearInterval, console, setInterval */
 
 /**
- * Add two numbers
- * @customfunction
- * @param {number} first First number
- * @param {number} second Second number
- * @returns {number} The sum of the two numbers.
- */
-export function add(first, second) {
-  return first + second;
-}
-
-/**
- * Displays the current time once a second
- * @customfunction
- * @param {CustomFunctions.StreamingInvocation<string>} invocation Custom function invocation
- */
-export function clock(invocation) {
-  const timer = setInterval(() => {
-    const time = currentTime();
-    invocation.setResult(time);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Returns the current time
- * @returns {string} String with the current time formatted for the current locale.
- */
-export function currentTime() {
-  return new Date().toLocaleTimeString();
-}
-
-/**
- * Increments a value once a second.
- * @customfunction
- * @param {number} incrementBy Amount to increment
- * @param {CustomFunctions.StreamingInvocation<number>} invocation
- */
-export function increment(incrementBy, invocation) {
-  let result = 0;
-  const timer = setInterval(() => {
-    result += incrementBy;
-    invocation.setResult(result);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Writes a message to console.log().
- * @customfunction LOG
- * @param {string} message String to write.
- * @returns String to write.
- */
-export function logMessage(message) {
-  console.log(message);
-
-  return message;
+  * Gets the stock price at the end of month.
+  * @customfunction
+  * @param {string} symbol Xueqiu stock symbol
+  * @param {number} year Year
+  * @param {number} month Month
+  * @return {number} The stock price at the end of month
+  */
+export async function getStockPrice(symbol, year, month) {
+  try {
+    const begin = (new Date()).getTime() + 86400000;
+    const url = "https://stock.xueqiu.com/v5/stock/chart/kline.json?symbol="+symbol+"&begin="+begin+"&period=month&type=before&count=-284&indicator=kline,pe,pb,ps,pcf,market_capital,agt,ggt,balance"
+    const response = await fetch(
+      url,
+      {
+        "headers": {
+          "accept": "application/json, text/plain, */*",
+          "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+          "sec-ch-ua": "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"macOS\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site"
+        },
+        "referrer": "https://xueqiu.com/S/" + symbol,
+        "referrerPolicy": "unsafe-url",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+      }
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    const json = await response.json();
+    const items = json["data"]["item"];
+    for (let index = items.length - 1; index >= 0; index--){
+      let date = new Date(items[index][0]);
+      if (date.getFullYear() === year && date.getMonth() === month) {
+        return items[index][5];
+      }
+    }
+  }
+  catch (error) {
+    return error;
+  }
 }
