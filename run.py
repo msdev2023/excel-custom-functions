444daf3109bb4434ad11ea6550ea9ccd03a46cb3
msdev2023 <125893591+msdev2023@users.noreply.github.com>
2023-04-28 14:43:31 +0800

    use headless chromedriver

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ import traceback
 from datetime import datetime
 
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 formatter = logging.Formatter(
     "[%(asctime)s] %(funcName)s:%(lineno)d %(levelname)s %(message)s"
@@ -16,165 +20,191 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 WEIBO_UID = os.environ.get("WEIBO_UID")
-WEIBO_COOKIE = os.environ.get("WEIBO_COOKIE")
+WEIBO_COOKIES = os.environ.get("WEIBO_COOKIES")
+WEIBO_LATEST_TIMESTAMP = os.environ.get("WEIBO_LATEST_TIMESTAMP")
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 GITHUB_REPOSITORY = os.environ.get("GITHUB_REPOSITORY")
 
-session = requests.Session()
-session.headers.update(
-    {
-        "authority": "weibo.com",
-        "accept": "application/json, text/plain, */*",
-        "accept-language": "zh-CN,zh;q=0.9",
-        "client-version": "v2.40.22",
-        "referer": f"https://weibo.com/u/{WEIBO_UID}",
-        "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "server-version": "v2023.04.04.2",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-        "x-requested-with": "XMLHttpRequest",
-    }
-)
-cookies = WEIBO_COOKIE.split("; ")
-for cookie in WEIBO_COOKIE.split():
-    k, v = cookie.split("=", 1)
-    session.cookies.set(k, v)
+
+class GitHubAPI:
+    def __init__(self, token):
+        self.headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+
+    def create_issue(self, title, body):
+        url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues"
+        data = {
+            "title": title,
+            "body": body,
+        }
+        resp = requests.post(url, data=json.dumps(data), headers=self.headers)
+        logger.info(resp.status_code)
+
+    def get_latest_timestamp(self):
+        url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/variables/WEIBO_LATEST_TIMESTAMP"
+        resp = requests.get(url, headers=self.headers)
+        logger.info(resp.status_code)
+        return resp.json()["value"]
+
+    def update_latest_timestamp(self, ts):
+        url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/variables/WEIBO_LATEST_TIMESTAMP"
+        data = {
+            "name": "WEIBO_LATEST_TIMESTAMP",
+            "value": str(ts),
+        }
+        resp = requests.patch(url, data=json.dumps(data), headers=self.headers)
+        logger.info(resp.status_code)
+
+    def update_weibo_cookies(self, cookies):
+        url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/variables/WEIBO_COOKIES"
+        data = {
+            "name": "WEIBO_COOKIES",
+            "value": json.dumps(cookies),
+        }
+        resp = requests.patch(url, data=json.dumps(data), headers=self.headers)
+        logger.info(resp.status_code)
 
 
-def start_request(uid, ts=None):
-    url = f"https://weibo.com/ajax/statuses/mymblog?uid={uid}&page=1"
-    resp = session.get(url)
-    logger.info(resp.status_code)
-    data = resp.json()
-    logger.info(data)
-    tweets = data["data"]["list"]
+class HeadlessWeibo:
+    def __init__(self):
+        options = Options()
+        options.add_argument("--headless")
+        # options.add_argument("--disable-dev-shm-usage")
+        # options.add_argument('--disable-infobars')
+        # options.add_argument("--no-sandbox")
+        # options.add_argument('--remote-debugging-port=9222')
+        self.driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()), options=options
+        )
 
-    latest_timestamp = None
-    for tweet in tweets:
-        try:
-            content, created_at = parse_tweet(uid, tweet)
-        except:
-            logger.error(traceback.print_exc())
-            continue
+        self.session = requests.Session()
+        self.session.headers.update(
+            {
+                "authority": "weibo.com",
+                "accept": "application/json, text/plain, */*",
+                "accept-language": "zh-CN,zh;q=0.9",
+                "client-version": "v2.40.22",
+                "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"macOS"',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "server-version": "v2023.04.04.2",
+                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+                "referer": "https://weibo.com",
+                # "x-requested-with": "XMLHttpRequest",
+            }
+        )
 
-        if ts and created_at <= ts:
-            break
+    def use_cookies(self, cookies):
+        self.driver.delete_all_cookies()
 
-        if not content:
-            continue
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
 
-        try:
-            gh_create_issue(content[:16], content)
-        except:
-            logger.error(traceback.print_exc())
-            continue
+    def get_cookies(self):
+        self.driver.get("https://weibo.com")
+        cookies = self.driver.get_cookies()
+        return cookies
 
-        if latest_timestamp is None or latest_timestamp < created_at:
-            latest_timestamp = created_at
+    def start_request(self, gh_api, uid, ts=None):
+        url = f"https://weibo.com/ajax/statuses/mymblog?uid={uid}&page=1"
+        self.session.headers.update(
+            {
+                "referer": f"https://weibo.com/u/{uid}",
+                "x-requested-with": "XMLHttpRequest",
+            }
+        )
+        resp = self.session.get(url)
+        logger.info(resp.status_code)
+        data = resp.json()
+        tweets = data["data"]["list"]
 
-    return latest_timestamp
+        latest_timestamp = None
+        for tweet in tweets:
+            try:
+                content, created_at = self.parse_tweet(uid, tweet)
+            except:
+                logger.error(traceback.print_exc())
+                continue
 
+            if ts and created_at <= ts:
+                break
 
-def parse_time(s):
-    """
-    Thu Apr 06 12:31:14 +0800 2023 => 1680755474
-    """
-    return int(datetime.strptime(s, "%a %b %d %H:%M:%S %z %Y").timestamp())
+            if not content:
+                continue
 
+            try:
+                title = content.split("\n")[0][:60]
+                gh_api.create_issue(title, content)
+            except:
+                logger.error(traceback.print_exc())
+                continue
 
-def parse_tweet(uid, tweet):
-    created_at = parse_time(tweet["created_at"])
+            if latest_timestamp is None or latest_timestamp < created_at:
+                latest_timestamp = created_at
 
-    if "repost_type" in tweet:
-        return None, created_at
+        return latest_timestamp
 
-    mblogid = tweet["mblogid"]
-    content = tweet["text_raw"]
-    if "continue_tag" in tweet and tweet["isLongText"]:
-        try:
-            long_url = f"https://weibo.com/ajax/statuses/longtext?id={mblogid}"
-            content = retrieve_long_tweet(long_url)
-        except:
-            logger.error(traceback.print_exc())
+    @staticmethod
+    def parse_time(s):
+        """
+        Thu Apr 06 12:31:14 +0800 2023 => 1680755474
+        """
+        return int(datetime.strptime(s, "%a %b %d %H:%M:%S %z %Y").timestamp())
 
-    content = content.replace("\u200b", "")
-    pic_urls = [
-        f"![](https://wx1.sinaimg.cn/orj960/{pic_id})"
-        for pic_id in tweet.get("pic_ids", [])
-    ]
-    tweet_url = f"https://weibo.com/{uid}/{mblogid}"
-    content += "\n" + "\n".join(pic_urls) + "\n\n" + tweet_url
-    return content, created_at
+    def parse_tweet(self, uid, tweet):
+        created_at = self.parse_time(tweet["created_at"])
 
+        if "repost_type" in tweet:
+            return None, created_at
 
-def retrieve_long_tweet(url):
-    resp = session.get(url)
-    logger.info(resp.status_code)
-    data = resp.json()
-    return data["data"]["longTextContent"]
+        mblogid = tweet["mblogid"]
+        content = tweet["text_raw"]
+        if "continue_tag" in tweet and tweet["isLongText"]:
+            try:
+                long_url = f"https://weibo.com/ajax/statuses/longtext?id={mblogid}"
+                content = self.retrieve_long_tweet(long_url)
+            except:
+                logger.error(traceback.print_exc())
 
+        content = content.replace("\u200b", "")
+        pic_urls = [
+            f"![](https://wx1.sinaimg.cn/orj960/{pic_id})"
+            for pic_id in tweet.get("pic_ids", [])
+        ]
+        tweet_url = f"https://weibo.com/{uid}/{mblogid}"
+        content += "\n" + "\n".join(pic_urls) + "\n\n" + tweet_url
+        return content, created_at
 
-def gh_create_issue(title, body):
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues"
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
-    data = {
-        "title": title,
-        "body": body,
-    }
-    resp = requests.post(url, data=json.dumps(data), headers=headers)
-    logger.info(resp.status_code)
-
-
-def gh_get_latest_timestamp():
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/variables/WEIBO_LATEST_TIMESTAMP"
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
-    resp = requests.get(url, headers=headers)
-    logger.info(resp.status_code)
-    return resp.json()["value"]
-
-
-def gh_update_latest_timestamp(ts):
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/variables/WEIBO_LATEST_TIMESTAMP"
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
-    data = {
-        "name": "WEIBO_LATEST_TIMESTAMP",
-        "value": str(ts),
-    }
-    resp = requests.patch(url, data=json.dumps(data), headers=headers)
-    logger.info(resp.status_code)
+    def retrieve_long_tweet(self, url):
+        resp = self.session.get(url)
+        logger.info(resp.status_code)
+        data = resp.json()
+        return data["data"]["longTextContent"]
 
 
 def main():
-    ts = None
-    try:
-        ts = gh_get_latest_timestamp()
-        ts = int(ts)
-    except:
-        logger.error(traceback.print_exc())
+    headless = HeadlessWeibo()
+    headless.use_cookies(json.loads(WEIBO_COOKIES))
+    new_cookies = headless.get_cookies()
 
-    latest_ts = start_request(WEIBO_UID, ts=ts)
+    gh_api = GitHubAPI(token=GITHUB_TOKEN)
+    gh_api.update_weibo_cookies(new_cookies)
 
-    try:
-        gh_update_latest_timestamp(latest_ts)
-    except:
-        logger.error(traceback.print_exc())
+    for cookie in new_cookies:
+        headless.session.cookies.set(cookie["name"], cookie["value"])
+
+    new_ts = headless.start_request(gh_api, WEIBO_UID, ts=int(WEIBO_LATEST_TIMESTAMP))
+    gh_api.update_latest_timestamp(new_ts)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except:
+        logger.error(traceback.print_exc())
